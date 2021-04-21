3d416ff15f096ef135b7567822269e7531866729
Jose Carlos del Castillo Estrada <[jose.carlos4013@gmail.com]>
2021-04-21 12:03:41 -0700

    Creado las conexiones de valor diferente para funcion 1 y 2

--- Prototipo1/ViewController.swift
+++ Prototipo1/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         Funciones(tituloFunc: "Funcion 1"),
         Funciones(tituloFunc: "Funcion 2")
     ]
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var lbFunc1Var1 = "dato_1"
+    var lbFunc1Var2 = "dato_2"
+    var lbFunc1Val1 = "10"
+    var lbFunc1Val2 = "15"
+    
+    var lbFunc2Var1 = "valor_1"
+    var lbFunc2Var2 = "valor_2"
+    var lbFunc2Val1 = "18"
+    var lbFunc2Val2 = "22"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,6 +42,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "celda")!
         cell.textLabel?.text = listaFunciones[indexPath.row].tituloFunc
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160
+    }
+    
+    
+    
+    // MARK: - Metodos de Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "muestraCodigo"{
+            let vistaCodigo = segue.destination as! VisualCodeViewController
+            
+            let indice = tableView.indexPathForSelectedRow!
+            
+            if listaFunciones[indice.row].tituloFunc == "Funcion 1"{
+                vistaCodigo.lbFuncVar1 = "dato_1"
+                vistaCodigo.lbFuncVar2 = "dato_2"
+                vistaCodigo.lbFuncVal1 = "10"
+                vistaCodigo.lbFuncVal2 = "15"
+            }
+            
+            else if listaFunciones[indice.row].tituloFunc == "Funcion 2"{
+                vistaCodigo.lbFuncVar1 = "valor_1"
+                vistaCodigo.lbFuncVar2 = "valor_2"
+                vistaCodigo.lbFuncVal1 = "18"
+                vistaCodigo.lbFuncVal2 = "22"
+            }
+            vistaCodigo.funcSelecc = listaFunciones[indice.row]
+        }
     }
 
 
