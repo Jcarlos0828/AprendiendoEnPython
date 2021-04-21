bba6e77cb81c269ff4a4448a7bf5e5ffcfbaa145
goldinio1963 <ablermav@hotmail.com>
2021-04-21 15:11:14 -0700

    se agrego un tap gesture y se modificaron las conexiones, al igual que el inicio del glosario

--- Prototipo1/ViewControllerGlosario.swift
+++ Prototipo1/ViewControllerGlosario.swift
@@ -11,10 +11,20 @@ class ViewControllerGlosario: UIViewController {
 
     @IBOutlet weak var lbGlosario: UILabel!
     
+    //var menu = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //lbGlosario.text
+        let str = """
+                    Bienvenidos al Glosario de Python
+                    
+                    ¿Que es def en Pyhton?
+                    Bueno en pyhton para poder definir una funcion se usa el def nombre_de_la_funcion(parametros):, el : es lo que usa Python para indicar que empieza la definicion.
+                    Para despues llamar una funcion declarada solo se usa nombre_de_la_funcion(parametros)
+                    """
+        
+        lbGlosario.text = str
 
         // Do any additional setup after loading the view.
     }
@@ -25,6 +35,7 @@ class ViewControllerGlosario: UIViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
--- Prototipo1/VisualCodeViewController.swift
+++ Prototipo1/VisualCodeViewController.swift
@@ -9,12 +9,54 @@ import UIKit
 
 class VisualCodeViewController: UIViewController {
 
+    @IBOutlet weak var lbTituloFunc: UILabel!
+    
+    
+    @IBOutlet weak var lbVariable1: UILabel!
+    @IBOutlet weak var lbvalorVariable_1: UILabel!
+    @IBOutlet weak var tfvalorVariable_1: UITextField!
+    
+    @IBOutlet weak var lbVariable2: UILabel!
+    @IBOutlet weak var tfvalorVariable_2: UITextField!
+    @IBOutlet weak var lbvalorVariable_2: UILabel!
+    
+    var funcSelecc : Funciones!
+    var lbFuncVar1 : String!
+    var lbFuncVar2 : String!
+    var lbFuncVal1 : String!
+    var lbFuncVal2 : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        lbTituloFunc.text = funcSelecc.tituloFunc
+        
+        lbVariable1.text = lbFuncVar1
+        lbVariable2.text = lbFuncVar2
+        lbvalorVariable_1.text = lbFuncVal1
+        lbvalorVariable_2.text = lbFuncVal2
+        
+        tfvalorVariable_1.isHidden = true
+        tfvalorVariable_2.isHidden = true
     }
     
+    @IBAction func cambiaModo(_ sender: UISegmentedControl) {
+        lbvalorVariable_1.isHidden = !lbvalorVariable_1.isHidden
+        lbvalorVariable_2.isHidden = !lbvalorVariable_2.isHidden
+        
+        tfvalorVariable_1.isHidden = !tfvalorVariable_1.isHidden
+        tfvalorVariable_2.isHidden = !tfvalorVariable_2.isHidden
+    }
+    
+    
+    @IBAction func quitarTeclado(_ sender: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func unwindGlosarioFuncion(segue: UIStoryboardSegue){
+        
+    }
 
     /*
     // MARK: - Navigation
